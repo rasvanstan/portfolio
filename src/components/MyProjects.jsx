f9ba108bb995d05ed0b8e02cce546f13234fd2a0
Razvan Stan <rasvan.stan@yahoo.com>
2024-04-11 10:17:44 +0200

    solar system h1 added

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -25,8 +25,11 @@ function MyProjects() {
           height: '100vh',
         }}
       >
-        <div className='My-Projects'>
+        <div className='my-projects'>
           <h1>My Projects</h1>
+          <div>
+            <h1>The solar system</h1>
+          </div>
           <p>This is where you can showcase your projects.</p>
         </div>
       </div>
