433ccbf4cc903c18b8bc6b50d61c62fc699932a5
Razvan Stan <rasvan.stan@yahoo.com>
2024-04-16 11:13:20 +0200

    footer modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import "./styles/App.css"
-import NavBar from './components/NavBar';
+import LeftNavBar from './components/LeftNavBar';
 import Home from './components/Home';
 import MyProjects from './components/MyProjects';
 import About from './components/About';
 import Contact from './components/Contact';
 import { BackgroundProvider } from './components/BackgroundContext';
-import Footer from './components/Footer';
+import RightNavBar from './components/RightNavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
    return (
      <BackgroundProvider>
        <Router>
-           <NavBar />
+           <LeftNavBar />
          <Routes>
            <Route path="/" element={<Home />} />
            <Route path="/projects" element={<MyProjects />} />
            <Route path="/about" element={<About />} />
            <Route path="/contact" element={<Contact />} />
          </Routes>
-         <Footer />
+         <RightNavBar />
        </Router>
      </BackgroundProvider>
    );
