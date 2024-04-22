98ee3082aa6692645eaa145bbac58750b008eabb
Razvan Stan <rasvan.stan@yahoo.com>
2024-04-22 16:31:35 +0200

    margin added in home page

--- src/components/BackgroundContext.jsx
+++ src/components/BackgroundContext.jsx
@@ -33,6 +33,7 @@ export const BackgroundProvider = ({ children }) => {
   useEffect(() => {
     // Whenever the path changes, trigger a background change
     changeTheme();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPath]);
 
   const changeTheme = () => {
