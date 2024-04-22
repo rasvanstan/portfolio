a7e3c0e13055352efb08ef0e64e9e78aaea32164
Razvan Stan <rasvan.stan@yahoo.com>
2024-04-22 18:28:01 +0200

    adjusted the code in my projects

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,79 +1,65 @@
+import React from 'react';
 import { useContext } from 'react';
-import { useState } from "react";
 import BackgroundContext from './BackgroundContext';
-import '../styles/Myprojects.css'
-import solarsystem from '/assets/solarsystem.png'
-import reactify from '/assets/reactify.png'
-import talkbuddy from '/assets/talkbuddy.png'
+import '../styles/Myprojects.css';
+import solarsystem from '/assets/solarsystem.png';
+import reactify from '/assets/reactify.png';
+import talkbuddy from '/assets/talkbuddy.png';
 
 function MyProjects() {
-  const { currentPhotoIndex, photos } = useContext(BackgroundContext);
-  const [isOpen, setIsOpen] = useState({
-    solarSystem: false,
-    spotifyClone: false,
-    talkBuddy: false
-  });
+ const { currentPhotoIndex, photos } = useContext(BackgroundContext);
 
-  return (
+ // Define an array of projects
+ const projects = [
+    {
+      title: 'The Solar System',
+      imgSrc: solarsystem,
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia'
+    },
+    {
+      title: 'Spotify Clone',
+      imgSrc: reactify,
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia'
+    },
+    {
+      title: 'Talk Buddy',
+      imgSrc: talkbuddy,
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia'
+    }
+ ];
+
+ return (
     <div className="main-container">
       <img
         src={photos[currentPhotoIndex]}
         alt="Background"
         loading="lazy"
-        style={{
-          display: 'none', // Hide the actual image, it's used for lazy loading
-        }}
+        style={{ display: 'none' }}
       />
       <div
         className="projects-container"
-        style={{
-          backgroundImage: `url(${photos[currentPhotoIndex]})`,
-        }}
+        style={{ backgroundImage: `url(${photos[currentPhotoIndex]})` }}
       >
-        <div className='grid-container'>
-          <div className='solar-system-container grid-item'>
-            <a href="https://myuniverseanimation.netlify.app/" target="_blank" rel="noopener noreferrer">
-              <img src={solarsystem} className='solar-system'/>
-            </a>
-          </div>
-          <div className='info-container grid-item'>
-            <section>
-              <div className="content">
-                <h1>The Solar System</h1>
-                <p className='solar-system-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia</p>
+        <div className="grid-container">
+          {projects.map((project, index) => (
+            <React.Fragment key={index}>
+              <div className="solar-system-container grid-item">
+                <img src={project.imgSrc} className="solar-system" />
               </div>
-            </section>
-          </div>
-          <div className='spotify-clone-container grid-item'>
-            <a href="https://reactify-chi.vercel.app/" target="_blank" rel="noopener noreferrer">
-              <img src={reactify} className='spotify-clone'/>
-            </a>
-          </div>
-          <div className='info-container grid-item'>
-            <section>
-              <div className="content">
-                <h1>Spotify Clone</h1>
-                <p className='solar-system-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia</p>
+              <div className="info-container grid-item">
+                <section>
+                 <div className="content">
+                    <h1>{project.title}</h1>
+                    <p className="solar-system-text">{project.text}</p>
+                 </div>
+                </section>
               </div>
-            </section>
-          </div>
-          <div className='talk-buddy-container grid-item'>
-            <a href="https://talk-buddy-client.onrender.com/" target="_blank" rel="noopener noreferrer">
-              <img src={talkbuddy} className='talk-buddy' style={{backgroundSize:'cover'}}/>
-            </a>
-          </div>
-          <div className='info-container grid-item'>
-            <section>
-              <div className="content">
-                <h1>Talk Buddy</h1>
-                <p className='solar-system-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia</p>
-              </div>
-            </section>
-          </div>
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </div>
-  );
+ );
 }
 
 export default MyProjects;
