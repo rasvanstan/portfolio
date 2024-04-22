c56fe8c4dda7dc6603b4ab6e5698690914877308
Razvan Stan <rasvan.stan@yahoo.com>
2024-04-22 17:58:01 +0200

    my projects updated

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import { useState } from "react";
-import { motion } from "framer-motion";
 import BackgroundContext from './BackgroundContext';
 import '../styles/Myprojects.css'
 
@@ -12,15 +11,10 @@ function MyProjects() {
     talkBuddy: false
   });
 
-  const handleToggle = (project) => {
-    setIsOpen({
-      ...isOpen,
-      [project]: !isOpen[project]
-    });
-  };
+ 
 
   return (
-    <div>
+    <div className="main-container">
       <img
         src={photos[currentPhotoIndex]}
         alt="Background"
@@ -30,53 +24,62 @@ function MyProjects() {
         }}
       />
       <div
+        className="projects-container"
         style={{
           backgroundImage: `url(${photos[currentPhotoIndex]})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center center',
-          width: '100vw',
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column', // Make the container flex column
-          justifyContent: 'flex-end' // Align items to the bottom
         }}
       >
-        <motion.div 
-          className='solar-system'
-          layout
-          data-isOpen={isOpen.solarSystem}
-          initial={{ borderRadius: 50 }}
-          onClick={() => handleToggle('solarSystem')}
-          style={{ marginBottom: isOpen.solarSystem ? 'auto' : 'none' }} // Move it to the top when opened
-        >
-          <h1>The Solar System</h1>
-          <motion.div layout className="child"></motion.div>
-        </motion.div>
-        
-        <motion.div 
-          className='spotify-clone'
-          layout
-          data-isOpen={isOpen.spotifyClone}
-          initial={{ borderRadius: 50 }}
-          onClick={() => handleToggle('spotifyClone')}
-          style={{ marginBottom: isOpen.spotifyClone ? 'auto' : 'none' }} // Move it to the top when opened
-        >
-          <h1>Spotify Clone</h1>
-          <motion.div layout className="child"></motion.div>
-        </motion.div>
-        
-        <motion.div 
-          className='talk-buddy'
-          layout
-          data-isOpen={isOpen.talkBuddy}
-          initial={{ borderRadius: 50 }}
-          onClick={() => handleToggle('talkBuddy')}
-          style={{ marginBottom: isOpen.talkBuddy ? 'auto' : 'none' }} // Move it to the top when opened
-        >
-          <h1>Talk Buddy</h1>
-          <motion.div layout className="child"></motion.div>
-        </motion.div>
+        <div className='grid-container'>
+          <div className='solar-system-container grid-item'>
+            <div
+              className='solar-system'
+            >
+              <div className="child"></div>
+            </div>
+          </div>
+          <div className='info-container grid-item'>
+            <section>
+              <div className="content">
+                <h1>The Solar System</h1>
+                <p className='solar-system-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia</p>
+              </div>
+            </section>
+          </div>
+          <div className='spotify-clone-container grid-item'>
+            <div
+              className='spotify-clone'
+              data-isOpen={isOpen.spotifyClone}
+              onClick={() => handleToggle('spotifyClone')}
+            >
+              <div className="child"></div>
+            </div>
+          </div>
+          <div className='info-container grid-item'>
+            <section>
+              <div className="content">
+                <h1>Spotify Clone</h1>
+                <p className='solar-system-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia</p>
+              </div>
+            </section>
+          </div>
+          <div className='talk-buddy-container grid-item'>
+            <div
+              className='talk-buddy'
+              data-isOpen={isOpen.talkBuddy}
+              onClick={() => handleToggle('talkBuddy')}
+            >
+              <div className="child"></div>
+            </div>
+          </div>
+          <div className='info-container grid-item'>
+            <section>
+              <div className="content">
+                <h1>Talk Buddy</h1>
+                <p className='solar-system-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitiaLorem ipsum, dolor sit amet consectetur adipisicing elit. Provident voluptas, labore molestiae, porro dolorem ea accusantium explicabo minima error, fuga enim ipsum ipsa mollitia eveniet accusamus voluptates! Distinctio, quia mollitia</p>
+              </div>
+            </section>
+          </div>
+        </div>
       </div>
     </div>
   );
